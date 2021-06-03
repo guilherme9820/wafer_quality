4cf8d31b2979429a23b2951fcedcbd759c19459c
Guilherme Henrique dos Santos <dos_santos_98@hotmail.com>
2021-06-03 18:30:09 -0300

    Added configuration files

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ setup(
     name='src',
     packages=find_packages(),
     version='0.1.0',
-    description='Project for 'MLOPS End to End Implementation' course.',
+    description="Project for 'MLOPS End to End Implementation' course.",
     author='guilherme',
     license='MIT',
 )
